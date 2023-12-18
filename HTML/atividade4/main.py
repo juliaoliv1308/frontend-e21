c3361f6eb5b282120c99d75c18794e7f8f41dda9
arthur h. erhardt <ryanwds101@gmail.com>
2023-12-18 16:30:21 -0300

    main.py

---
+++ HTML/atividade4/main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+lista_produtos = []
+
+
+@app.route("/")
+def inicio():
+    return render_template('inicio.html')
+
+@app.route("/create")
+def create():
+    return render_template('cadastro.html')
+
+@app.route('/save', methods=["POST"])
+def save():
+    nome = request.form.get("nome")
+    descricao = request.form.get("descricao")
+    valor = request.form.get("valor")
+    prod = {"nome":nome, "descricao": descricao, "valor": valor}
+    lista_produtos.append(prod)
+    return redirect('/list')
+
+
+@app.route("/list")
+def list():
+    return render_template('listagem.html', lista = lista_produtos)
+
+app.run(debug=True)
